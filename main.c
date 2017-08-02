46d257ddb5acf2e4b3846b7c3857eb72084e68c1
Seb <seebzei@gmail.com>
2017-08-02 23:47:03 +0200

    La til testfil og mangler looping

--- main.c
+++ main.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
 
+FILE *filePath;
+
+int Cells[256];
+int CellPos = 0;
 int main()
-{
-	printf("hello world\n");
+{   
+    for(int i = 0; i < sizeof(Cells); i++)
+    {
+        Cells[i] = 0;
+    }
+	char fileName[64];
+	if(fgets(fileName, sizeof(fileName), stdin))
+	{
+		printf("Du skrev %s", fileName );
+		filePath = fopen("D:/Prosjekter/C/Brainfucktolk/test.bf", "r");
+		if(filePath)
+		{
+			char c;
+			while((c = getc(filePath)) != EOF)
+			{
+				putchar(c);
+                switch(c)
+                {
+                    case '>':
+                        if((CellPos + 1) < 254)
+                            CellPos++;
+                    break;
+                    case '<':
+                        if((CellPos - 1) >= 0)
+                            CellPos--;
+                    break;
+                    case '+':
+                        Cells[CellPos]++;
+                    break;
+                    case '-':
+                        Cells[CellPos]--;
+                    break;
+                    case '.':
+                        putchar(Cells[CellPos]);
+                    break;
+                    case ',':
+                        Cells[CellPos] = getchar();
+                        putchar(Cells[CellPos]);
+                    break;
+                    case '[':
+                    break;
+                    case ']':
+                    break;
+                }
+			}
+		}
+		else
+			puts("Ingen fil å lese yo");
+		fclose(filePath);
+	}
 	return 0;
 }
